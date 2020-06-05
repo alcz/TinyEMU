cbb8204f5606f808b2c2b430b511cec8c66b8ca5
Aleksander Czajczynski <hb@fki.pl>
2020-06-05 09:42:14 +0200

    updated for recent emscripten: Runtime.dynCall -> dynCall, Pointer_stringify -> UTF8String plus some linker exports

--- Makefile.js
+++ Makefile.js
@@ -22,23 +22,39 @@
 # THE SOFTWARE.
 #
 
+# Build x86 emulator
+CONFIG_X86EMU=y
+
 # Build the Javascript version of TinyEMU
 EMCC=emcc
 EMCFLAGS=-O2 --llvm-opts 2 -Wall -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -MMD -fno-strict-aliasing -DCONFIG_FS_NET
 #EMCFLAGS+=-Werror
-EMLDFLAGS=-O3 --memory-init-file 0 --closure 0 -s NO_EXIT_RUNTIME=1 -s NO_FILESYSTEM=1 -s "EXPORTED_FUNCTIONS=['_console_queue_char','_vm_start','_fs_import_file','_display_key_event','_display_mouse_event','_display_wheel_event','_net_write_packet','_net_set_carrier']" -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]' -s BINARYEN_TRAP_MODE=clamp --js-library js/lib.js
+EMLDFLAGS=-O3 --memory-init-file 0 --closure 0 -s NO_EXIT_RUNTIME=1 -s NO_FILESYSTEM=1 -s "EXPORTED_FUNCTIONS=['_console_queue_char','_vm_start','_fs_import_file','_display_key_event','_display_mouse_event','_display_wheel_event','_net_write_packet','_net_set_carrier']" -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap", "UTF8ToString"]'  --js-library js/lib.js
 EMLDFLAGS_ASMJS:=$(EMLDFLAGS) -s WASM=0
 EMLDFLAGS_WASM:=$(EMLDFLAGS) -s WASM=1 -s TOTAL_MEMORY=67108864 -s ALLOW_MEMORY_GROWTH=1
 
-PROGS=js/riscvemu32.js js/riscvemu32-wasm.js js/riscvemu64.js js/riscvemu64-wasm.js
+# -s BINARYEN_TRAP_MODE=clamp
+
+PROGS=js/x86emu.js js/x86emu-wasm.js js/riscvemu32.js js/riscvemu32-wasm.js js/riscvemu64.js js/riscvemu64-wasm.js
 
 all: $(PROGS)
 
 JS_OBJS=jsemu.js.o softfp.js.o virtio.js.o fs.js.o fs_net.js.o fs_wget.js.o fs_utils.js.o simplefb.js.o pci.js.o json.js.o block_net.js.o
 JS_OBJS+=iomem.js.o cutils.js.o aes.js.o sha256.js.o
 
+ifdef CONFIG_X86EMU
+EMCFLAGS+=-DCONFIG_X86EMU
+endif
+
+X86_OBJS=$(JS_OBJS) x86_cpu.js.o x86_machine.js.o ide.js.o ps2.js.o vmmouse.js.o pckbd.js.o vga.js.o machine.js.o
 RISCVEMU64_OBJS=$(JS_OBJS) riscv_cpu64.js.o riscv_machine.js.o machine.js.o
 RISCVEMU32_OBJS=$(JS_OBJS) riscv_cpu32.js.o riscv_machine.js.o machine.js.o
+
+js/x86emu.js: $(X86_OBJS) js/lib.js
+	$(EMCC) $(EMLDFLAGS_ASMJS) -o $@ $(X86_OBJS)
+
+js/x86emu-wasm.js: $(X86_OBJS) js/lib.js
+	$(EMCC) $(EMLDFLAGS_WASM) -o $@ $(X86_OBJS)
 
 js/riscvemu64.js: $(RISCVEMU64_OBJS) js/lib.js
 	$(EMCC) $(EMLDFLAGS_ASMJS) -o $@ $(RISCVEMU64_OBJS)
@@ -57,7 +73,6 @@ riscv_cpu32.js.o: riscv_cpu.c
 
 riscv_cpu64.js.o: riscv_cpu.c
 	$(EMCC) $(EMCFLAGS) -DMAX_XLEN=64 -DCONFIG_RISCV_MAX_XLEN=64 -c -o $@ $<
-
 
 %.js.o: %.c
 	$(EMCC) $(EMCFLAGS) -c -o $@ $<
